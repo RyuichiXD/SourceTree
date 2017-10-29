e2fba7a63df89148fd1948b798a3c2b7fecd5288
Ryuichi001 <samed.sulanc@yahoo.de>
2017-10-29 20:03:22 +0100

    hccc

--- restful-api/server.js
+++ restful-api/server.js
@@ -138,8 +138,8 @@ var server = app.listen(8081, function () {
   var host = server.address().address
   var port = server.address().port
 
-  console.log("Example app listening at",host,':',port)
-  console.log("Test")
+  console.log("Example app listening at : ",host,':',port)
+  console.log("T3")
 
 
 })
